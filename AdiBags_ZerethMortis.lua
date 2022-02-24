4fb64c1542ebecfa5b5b8fd8afaf2f0a2a5267e2
Adam Koch <me@adamko.ch>
2022-02-23 22:29:57 -0500

    Added items from Wowhead 9.2 guide.

--- AdiBags_ZerethMortis.lua
+++ AdiBags_ZerethMortis.lua
@@ -29,31 +29,79 @@ shardFilter.uiDesc = L["Items relating to Zereth Mortis and patch 9.2."]
 
 function shardFilter:OnInitialize()
     self.zerethMortis = {
+        -- From: https://www.wowhead.com/guides/rare-spawn-treasure-locations-loot-zereth-mortis
+
         -- Crafting Materials
         [187703] = true, -- Silken Protofiber
         [187704] = true, -- Protoflesh
-		
+        
         -- Cosmetic Transmog
         [190637] = true, -- Percussive Maintenance Instrument
+        [190952] = true, -- Protoflora Harvester
+        [190942] = true, -- Protomineral Extractor
+        [190638] = true, -- Tormented Mawsteel Greatsword
 
         -- Keys & Key Fragments
         [189863] = true, -- Spatial Opener
-		
-		-- Lore
-		[187810] = true, -- Cypher Lore Codex
-		
-		-- Toys
-		[190754] = true, -- Firim's Specimen Container
-		
-		-- Tales of the Exile Scrolls
-		[189575] = true, -- Firim in Exile, Part 1
-		[189576] = true, -- Firim in Exile, Part 2
-		[189578] = true, -- Firim in Exile, Part 3
-		[189579] = true, -- Firim in Exile, Part 4
-		[189580] = true, -- Firim in Exile, Part 5
-		[189581] = true, -- Firim in Exile, Part 6
-		[189582] = true, -- Firim in Exile, Part 7
-		[189753] = true, -- Firim in Exile, Epilogue
+        
+        -- Lore
+        [187810] = true, -- Cypher Lore Codex
+        
+        -- Miscellaneous
+        [190339] = true, -- Enlightened Offering
+        [190953] = true, -- Protofruit Flesh
+        [187662] = true, -- Strange Goop
+        [190189] = true, -- Sandworm Relic
+
+        -- Pocopoc Attire
+        [190061] = true, -- Admiral Pocopoc
+        [190060] = true, -- Adventurous Pocopoc
+        [189451] = true, -- Chef Pocopoc
+        [187833] = true, -- Dapper Pocopoc
+        [190058] = true, -- Peaceful Pocopoc
+        [190059] = true, -- Pirate Pocopoc
+        [190098] = true, -- Pepepec
+
+        -- Protoform Synthesis
+        [187634] = true, -- Ambystan Lattice
+        [187633] = true, -- Bufonid Lattice
+        [187635] = true, -- Cervid Lattice
+        [189146] = true, -- Geomental Lattice
+        [189159] = true, -- Glimmer of Discovery
+        [189145] = true, -- Helicid Lattice
+        [190388] = true, -- Lupine Lattice
+        [189176] = true, -- Protoform Sentience Crown
+        [189150] = true, -- Raptora Lattice
+        [189177] = true, -- Revelation Key
+        [189151] = true, -- Scarabid Lattice
+        [189152] = true, -- Tarachnid Lattice
+        [189153] = true, -- Unformed Lattice
+        [189154] = true, -- Vespoid Lattice
+        [189155] = true, -- Viperid Lattice
+        [189156] = true, -- Vombata Lattice
+
+        -- Schematics
+        [189478] = true, -- Schematic: Adorned Vombata
+        [189435] = true, -- Schematic: Multichicken
+        [189469] = true, -- Schematic: Prototype Leaper
+        [189456] = true, -- Schematic: Sundered Zerethsteed
+        [189447] = true, -- Schematic: Viperid Menace
+
+        -- Toys
+        [190853] = true, -- Bushel of Mysterious Fruit
+        [190754] = true, -- Firim's Specimen Container
+        [190926] = true, -- Infested Automa Core
+        [190457] = true, -- Protopological Cube
+        
+        -- Tales of the Exile Scrolls
+        [189575] = true, -- Firim in Exile, Part 1
+        [189576] = true, -- Firim in Exile, Part 2
+        [189578] = true, -- Firim in Exile, Part 3
+        [189579] = true, -- Firim in Exile, Part 4
+        [189580] = true, -- Firim in Exile, Part 5
+        [189581] = true, -- Firim in Exile, Part 6
+        [189582] = true, -- Firim in Exile, Part 7
+        [189753] = true, -- Firim in Exile, Epilogue
     }
 end
 
--- README.md
+++ README.md
@@ -3,3 +3,87 @@
 Adds a filter to AdiBags for items from Zereth Mortis, the new zone added in 9.2.
 
 [https://www.curseforge.com/wow/addons/adibags-zereth-mortis](https://www.curseforge.com/wow/addons/adibags-zereth-mortis)
+
+## Items Filtered
+
+### Crafting Materials
+
+* [Silken Protofiber](https://wowhead.com/item=187703)
+* [Protoflesh](https://wowhead.com/item=187704)
+
+### Cosmetic Transmog
+
+* [Percussive Maintenance Instrument](https://wowhead.com/item=190637)
+* [Protoflora Harvester](https://wowhead.com/item=190952)
+* [Protomineral Extractor](https://wowhead.com/item=190942)
+* [Tormented Mawsteel Greatsword](https://wowhead.com/item=190638)
+
+### Keys & Key Fragments
+
+* [Spatial Opener](https://wowhead.com/item=189863)
+
+### Lore
+
+* [Cypher Lore Codex](https://wowhead.com/item=187810)
+
+### Miscellaneous
+
+* [Enlightened Offering](https://wowhead.com/item=190339)
+* [Protofruit Flesh](https://wowhead.com/item=190953)
+* [Strange Goop](https://wowhead.com/item=187662)
+* [Sandworm Relic](https://wowhead.com/item=190189)
+
+### Pocopoc Attire
+
+* [Admiral Pocopoc](https://wowhead.com/item=190061)
+* [Adventurous Pocopoc](https://wowhead.com/item=190060)
+* [Chef Pocopoc](https://wowhead.com/item=189451)
+* [Dapper Pocopoc](https://wowhead.com/item=187833)
+* [Peaceful Pocopoc](https://wowhead.com/item=190058)
+* [Pirate Pocopoc](https://wowhead.com/item=190059)
+* [Pepepec](https://wowhead.com/item=190098)
+
+### Protoform Synthesis
+
+* [Ambystan Lattice](https://wowhead.com/item=187634)
+* [Bufonid Lattice](https://wowhead.com/item=187633)
+* [Cervid Lattice](https://wowhead.com/item=187635)
+* [Geomental Lattice](https://wowhead.com/item=189146)
+* [Glimmer of Discovery](https://wowhead.com/item=189159)
+* [Helicid Lattice](https://wowhead.com/item=189145)
+* [Lupine Lattice](https://wowhead.com/item=190388)
+* [Protoform Sentience Crown](https://wowhead.com/item=189176)
+* [Raptora Lattice](https://wowhead.com/item=189150)
+* [Revelation Key](https://wowhead.com/item=189177)
+* [Scarabid Lattice](https://wowhead.com/item=189151)
+* [Tarachnid Lattice](https://wowhead.com/item=189152)
+* [Unformed Lattice](https://wowhead.com/item=189153)
+* [Vespoid Lattice](https://wowhead.com/item=189154)
+* [Viperid Lattice](https://wowhead.com/item=189155)
+* [Vombata Lattice](https://wowhead.com/item=189156)
+
+### Schematics
+
+* [Schematic: Adorned Vombata](https://wowhead.com/item=189478)
+* [Schematic: Multichicken](https://wowhead.com/item=189435)
+* [Schematic: Prototype Leaper](https://wowhead.com/item=189469)
+* [Schematic: Sundered Zerethsteed](https://wowhead.com/item=189456)
+* [Schematic: Viperid Menace](https://wowhead.com/item=189447)
+
+### Toys
+
+* [Bushel of Mysterious Fruit](https://wowhead.com/item=190853)
+* [Firim's Specimen Container](https://wowhead.com/item=190754)
+* [Infested Automa Core](https://wowhead.com/item=190926)
+* [Protopological Cube](https://wowhead.com/item=190457)
+
+### Tales of the Exile Scrolls
+
+* [Firim in Exile, Part 1](https://wowhead.com/item=189575)
+* [Firim in Exile, Part 2](https://wowhead.com/item=189576)
+* [Firim in Exile, Part 3](https://wowhead.com/item=189578)
+* [Firim in Exile, Part 4](https://wowhead.com/item=189579)
+* [Firim in Exile, Part 5](https://wowhead.com/item=189580)
+* [Firim in Exile, Part 6](https://wowhead.com/item=189581)
+* [Firim in Exile, Part 7](https://wowhead.com/item=189582)
+* [Firim in Exile, Epilogue](https://wowhead.com/item=189753)
